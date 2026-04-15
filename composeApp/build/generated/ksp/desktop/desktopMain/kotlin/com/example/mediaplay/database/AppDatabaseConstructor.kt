7e911ee139cec4d37d41f92e34dbce6ec7ad154c
Jonas <jonasrafael01@gmail.com>
2026-04-15 16:59:06 -0300

    14tentantiva

--- composeApp/build/generated/ksp/desktop/desktopMain/kotlin/com/example/mediaplay/database/AppDatabaseConstructor.kt
+++
@@ -1,7 +0,0 @@
-package com.example.mediaplay.database
-
-import androidx.room.RoomDatabaseConstructor
-
-public actual object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
-  actual override fun initialize(): AppDatabase = com.example.mediaplay.database.AppDatabase_Impl()
-}
