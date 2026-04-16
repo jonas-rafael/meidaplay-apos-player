1085973d161a05c6066bc286ae3c32bb1d22185d
Jonas <jonasrafael01@gmail.com>
2026-04-16 08:28:55 -0300

    19tentantiva

--- composeApp/src/androidMain/kotlin/com/example/mediaplay/database/Database.kt
+++ composeApp/src/androidMain/kotlin/com/example/mediaplay/database/Database.kt
@@ -3,7 +3,6 @@ package com.example.mediaplay.database
 import android.content.Context
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.RoomDatabaseConstructor
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
 
 private lateinit var applicationContext: Context
@@ -12,16 +11,11 @@ fun initDatabaseContext(context: Context) {
     applicationContext = context
 }
 
-actual object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
-    actual override fun initialize(): AppDatabase = AppDatabase_Impl()
-}
-
 actual fun getDatabaseBuilder(): RoomDatabase.Builder<AppDatabase> {
     val dbFile = applicationContext.getDatabasePath("mediaplay.db")
     return Room.databaseBuilder<AppDatabase>(
         context = applicationContext,
-        name = dbFile.absolutePath,
-        factory = { AppDatabaseConstructor.initialize() }
+        name = dbFile.absolutePath
     ).setDriver(BundledSQLiteDriver())
         .fallbackToDestructiveMigrationOnDowngrade(true)
         .fallbackToDestructiveMigration(true)
